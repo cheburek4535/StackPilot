515e4d389e5c0c60a6379317021f7f3298985c03
= <=>
2026-08-26 15:48:46 +0300

    фикс конфигов

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,26 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
-const host = process.env.TAURI_DEV_HOST;
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [sveltekit()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: "0.0.0.0",
-    allowedHosts: true,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+export default defineConfig(async ({ mode }) => {
+  const host = process.env.TAURI_DEV_HOST;
+  return {
+    plugins: [sveltekit()],
+    clearScreen: false,
+    server: {
+      port: 1420,
+      strictPort: true,
+      host: "0.0.0.0",
+      allowedHosts: ["*"],
+      hmr: host
+        ? {
+            protocol: "ws",
+            host,
+            port: 1421,
+          }
+        : undefined,
+      watch: {
+        ignored: ["**/src-tauri/**"],
+      },
     },
-  },
+  };
 });
